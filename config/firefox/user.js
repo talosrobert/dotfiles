b57d24abbdd9fc3b7614b0bfbf4f7aeebffd0bd3
talosrobert <29042337+talosrobert@users.noreply.github.com>
2026-03-25 13:00:40 +0100

    feat/add-firefox-audio-configs

---
+++ config/firefox/user.js
@@ -0,0 +1,12 @@
+// Hardware Video Decoding (VAAPI)
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("widget.dmabuf.force-enabled", true);
+
+// GPU Compositing
+user_pref("gfx.webrender.all", true);
+user_pref("layers.gpu-process.enabled", true);
+
+// WebRTC Camera Performance
+user_pref("media.navigator.mediadatadecoder_vpx_enabled", true);
+user_pref("media.webrtc.camera.allow-pipewire", true);
